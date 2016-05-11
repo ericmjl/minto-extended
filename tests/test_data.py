2e0e1496ee1afdd518f0e50590b86b61984c39d4
Eric J. Ma <ericmajinglong@gmail.com>
2016-05-11 16:59:44 -0400

    checked for bad strain names

--- tests/test_data.py
+++ tests/test_data.py
@@ -3,13 +3,15 @@ import os
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-ird_df = pd.read_csv('../data/20160411_Minto_Run2 Sequences.csv')
+ird_df = pd.read_csv('../data/20160411_Minto_Run2 Sequences.csv',
+                     parse_dates=['Collection Date'])
 ird_df['Strain Name'] = ird_df['Strain Name'].str.split('(').str[0]
 ird_df['Host Species'] = ird_df['Host Species'].str.replace('IRD:', '')\
     .str.replace('/Avian', '')
 ird_df.head()
 
-submitted_df = pd.read_csv('../data/Alaska_waterfowl_79viruses_metadata_20151223.csv')
+submitted_df = pd.read_csv(
+    '../data/Alaska_waterfowl_79viruses_metadata_20151223.csv')
 submitted_df['Strain_name'].tail()
 
 
@@ -20,3 +22,23 @@ def test_all_strains_of_interest_are_present():
     """
     for r, d in submitted_df.iterrows():
         assert d['Strain_name'] in ird_df['Strain Name'].values
+
+
+def test_dates_are_in_correct_range():
+    """
+    Checks to make sure that all of the dates are within the 2008-2012 range
+    """
+    assert ird_df['Collection Date'].min().year == 2007
+    assert ird_df['Collection Date'].max().year == 2012
+
+
+def test_strain_names_and_host_speices_are_correct():
+    """
+    Checks to make sure that all of the strain names and their corresponding
+    host species are correctly matching. For example, cannot have:
+
+    A/green-winged_teal/... matched with Host Species: Mallard
+    """
+    
+    
+    
