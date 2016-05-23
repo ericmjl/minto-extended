31607a71fa37f77dd37046c03d64060eb68568e2
Eric J. Ma <ericmajinglong@gmail.com>
2016-05-23 12:01:02 -0400

    got the data cleaning done right

--- scripts/data_clean.py
+++ scripts/data_clean.py
@@ -1,9 +1,21 @@
 import pandas as pd
 from Bio import SeqIO
+from tqdm import tqdm
 
 ird_df = pd.read_csv(
     '../data/20160411_Minto_Run2 Sequences.backup_20160518.csv')
 ird_df['Sequence Accession'] = ird_df['Sequence Accession'].str.strip('*')
+ird_df.set_index('Sequence Accession', inplace=True)
+
+corrected_df = pd.read_csv(
+    '../data/20160411_Minto_Run2 Sequences.corrected_dates_20160506.csv',
+    index_col=0)
+# corrected_df[corrected_df['Dates_Corrected_By_Brandt']]
+corrected_df['Sequence Accession'] = corrected_df['Sequence Accession']\
+    .str.strip('*')
+corrected_df.set_index('Sequence Accession', inplace=True)
+corrected_df = corrected_df[corrected_df['Date_Corrected_By_Brandt'] == '*']
+corrected_df.shape
 
 
 def clean_strains_with_bad_dates(ird_df):
@@ -13,10 +25,22 @@ def clean_strains_with_bad_dates(ird_df):
     ird_df['date_length'] = ird_df['Collection Date'].str.split('/').str.len()
     ird_df = ird_df[ird_df['date_length'] == 3]
 
-    ird_df.to_csv('../data/20160411_Minto_Run2 Sequences.csv')
+    return ird_df
 
 
-def filter_sequences_to_match_ird_data():
+def correct_dates_provided_by_brandt(corrected_df, ird_df):
+    """
+    Brandt has provided a set of dates that are corrected. This function will
+    modify the dates in ird_df using the dates in corrected_df.
+    """
+    print('Correcting dates provided by Brandt...')
+    for r, d in tqdm(corrected_df.iterrows()):
+        ird_df.set_value(r, 'Collection Date', d['Collection Date'])
+
+    return ird_df
+
+
+def filter_sequences_to_match_ird_data(ird_df):
     """
     Because of the data removal involved in cleaning strains with bad dates, I
     need another function that ensures that only the strains left behind are
@@ -27,15 +51,26 @@ def filter_sequences_to_match_ird_data():
             '../data/20160411_Minto_Run2 Sequences.backup_20160518.fasta',
             'fasta'))
 
+    print('Filtering sequences...')
     filtered_sequences = dict()
-    for k, v in ird_sequences.items():
-        if k in ird_df['Sequence Accession'].values:
+    for k, v in tqdm(ird_sequences.items()):
+        if k in ird_df.index:
             filtered_sequences[k] = v
 
+    print('Final number of sequences:')
+    print(len(filtered_sequences))
     SeqIO.write(filtered_sequences.values(),
                 '../data/20160411_Minto_Run2 Sequences.fasta',
                 'fasta')
 
+
+def save_data(ird_df):
+    ird_df.reset_index(inplace=True)
+    ird_df.to_csv('../data/20160411_Minto_Run2 Sequences.csv')
+
+
 if __name__ == '__main__':
-    clean_strains_with_bad_dates(ird_df)
-    filter_sequences_to_match_ird_data()
+    ird_df = correct_dates_provided_by_brandt(corrected_df, ird_df)
+    ird_df = clean_strains_with_bad_dates(ird_df)
+    filter_sequences_to_match_ird_data(ird_df)
+    save_data(ird_df)
